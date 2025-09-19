5f087ff61daf9cc3e6f7d0f9f3f0dd52d0237927
Rohit <rohitreddy2325@gmail.com>
2025-09-19 18:51:03 +0530

    you can show the points from the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { useState } from "react";
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function App() {
+  const [markers, setMarkers] = useState([]);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyBVFd5hkH3OnkuFGN2s0VuOzUYz9g1JPOM', // replace with your API key
+    libraries: ['places']
+  });
+
+  const handleMapClick = (e) => {
+    const newMarker = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng()
+    };
+    console.log(newMarker);
+    setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+  };
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onClick={handleMapClick}
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker} />
+        ))}
+      </GoogleMap>
     </div>
   );
 }
